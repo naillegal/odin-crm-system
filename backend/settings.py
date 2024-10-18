2af7aba6dd0a9b3bf2982d0d9e7fdcab1c583292
naillegal <quluzade.nail@gmail.com>
2024-10-18 22:23:40 +0400

    .env configured to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from os import getenv
 import os   
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
